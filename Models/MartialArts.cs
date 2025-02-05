798f4a5cc5b8466f0bf5d30cc255882d4897116d
Haris Sabanovic <hasa2303@student.miun.se>
2025-02-05 11:51:06 +0100

    Skapade en modell för kampsporter

---
+++ Models/MartialArts.cs
@@ -0,0 +1,12 @@
+using System;
+namespace MyApp.Models
+{
+    public class MartialArts {
+        public int id {get; set;}
+        public string? MartialArtName {get; set;}
+
+        public string? Description {get; set;}
+
+        public string? Link {get; set;}
+    }
+}
