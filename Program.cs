d544eb330614eaf6383d5b415e615c85803a6724
Haris Sabanovic <hasa2303@student.miun.se>
2025-02-10 19:12:38 +0100

    lade till session sparande

--- Program.cs
+++ Program.cs
@@ -3,10 +3,25 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews(); // aktiverar MVC
 
 
+//används för att session ska fungera
+builder.Services.AddDistributedMemoryCache();
+
+//används också för att session ska fungera
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(2);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+
 var app = builder.Build();
 
 app.UseStaticFiles();
 app.UseRouting();
+
+//används också för att session ska fungera
+app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
